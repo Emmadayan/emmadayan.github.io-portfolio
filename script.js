c97f07e053a426d7cb5be83233fec9c47dbc1aea
Emmadayan <46944765+Emmadayan@users.noreply.github.com>
2021-04-20 21:06:12 +1000

    Uploaded skeleton

---
+++ script.js
@@ -0,0 +1,10 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+
+    M.AutoInit();
+
+  ScrollReveal().reveal('.headline', {reset: true });
+
+  $(".story-heading").remove()
+
+});
